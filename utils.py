d8378c92886e430027b49b774be613bc2d873f18
Gino Farisano <g.farisano@reply.it>
2021-06-25 12:54:51 +0200

    buonanotte
    buonanotte 2
    
    buonanotte final

--- utils.py
+++ utils.py
@@ -8,4 +8,13 @@ def func_buongiorno_3():
     print("buongiorno")
 
 def func_buongiorno_4():
-    print("buongiornissimo, kaffeeeee???")
+    print("buongiornissimo, kaffeeeee???"
+
+def func_buonanotte():
+    print("buonanotte")
+
+def func_buonanotte_2():
+    print("buonanotte vado ad letto")
+
+def func_buonanotte_3():
+    print("buonanotte mondo :-)")
